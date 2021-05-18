c5d1c6e387b2e2ad9e9343b868b197f5b63a065d
Vitor Moreno <vh.morenodasilva@gmail.com>
2021-05-18 13:47:00 -0300

    Fixed sql errors when creating simpleassets tables

--- definitions/migrations/1.2.3/database.sql
+++ definitions/migrations/1.2.3/database.sql
@@ -1,5 +1,9 @@
 ALTER TABLE contract_traces DROP CONSTRAINT IF EXISTS atomicassets_logs_pkey;
 ALTER TABLE contract_traces DROP CONSTRAINT IF EXISTS contract_traces_pkey;
-ALTER TABLE contract_traces ADD CONSTRAINT contract_traces_pkey PRIMARY KEY (global_sequence, account);
-
-UPDATE dbinfo SET "value" = '1.2.3' WHERE name = 'version';
+-- Fix multiple primary keys issue when creating simpleassets tables
+ALTER TABLE contract_traces DROP CONSTRAINT IF EXISTS contract_traces_pkey2;
+ALTER TABLE contract_traces
+ADD CONSTRAINT contract_traces_pkey PRIMARY KEY (global_sequence, account);
+UPDATE dbinfo
+SET "value" = '1.2.3'
+WHERE name = 'version';
--- definitions/tables/simpleassets_tables.sql
+++ definitions/tables/simpleassets_tables.sql
@@ -3,6 +3,7 @@ CREATE TABLE simpleassets_assets (
     asset_id bigint NOT NULL,
     author character varying(12) NOT NULL,
     category character varying(12) NOT NULL,
+    collection_name character varying(12) NULL,
     owner character varying(12),
     mutable_data jsonb,
     immutable_data jsonb,
@@ -17,16 +18,15 @@ CREATE TABLE simpleassets_assets (
     minted_at_time bigint NOT NULL,
     CONSTRAINT simpleassets_assets_pkey PRIMARY KEY (contract, asset_id)
 );
-
 CREATE TABLE simpleassets_authors (
     contract character varying(12) NOT NULL,
     author character varying(12) NOT NULL,
     dappinfo jsonb,
     fieldtypes jsonb,
     priorityimg jsonb,
-    CONSTRAINT simpleassets_collections_pkey PRIMARY KEY (contract, collection_name)
+    created_at_block bigint NOT NULL,
+    created_at_time bigint NOT NULL
 );
-
 CREATE TABLE simpleassets_transfers (
     transfer_id bigint NOT NULL,
     contract character varying(12) NOT NULL,
@@ -38,7 +38,6 @@ CREATE TABLE simpleassets_transfers (
     created_at_time bigint NOT NULL,
     CONSTRAINT simpleassets_transfers_pkey PRIMARY KEY (transfer_id)
 );
-
 CREATE TABLE simpleassets_transfers_assets (
     transfer_id bigint NOT NULL,
     contract character varying(12) NOT NULL,
@@ -46,21 +45,16 @@ CREATE TABLE simpleassets_transfers_assets (
     asset_id bigint NOT NULL,
     CONSTRAINT simpleassets_transfers_assets_pkey PRIMARY KEY (transfer_id, contract, asset_id)
 );
-
 CREATE TABLE simpleassets_config (
     contract character varying(12) NOT NULL,
     version character varying(64) NOT NULL,
     CONSTRAINT simpleassets_config_pkey PRIMARY KEY (contract)
 );
-
-
 -- FOREIGN KEYS --
 ALTER TABLE ONLY simpleassets_transfers_assets
-    ADD CONSTRAINT simpleassets_transfers_assets_assets_fkey FOREIGN KEY (asset_id, contract) REFERENCES simpleassets_assets(asset_id, contract) MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED NOT VALID;
-
+ADD CONSTRAINT simpleassets_transfers_assets_assets_fkey FOREIGN KEY (asset_id, contract) REFERENCES simpleassets_assets(asset_id, contract) MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED NOT VALID;
 ALTER TABLE ONLY simpleassets_transfers_assets
-    ADD CONSTRAINT simpleassets_transfers_assets_transfers_fkey FOREIGN KEY (transfer_id) REFERENCES simpleassets_transfers(transfer_id) MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED NOT VALID;
-
+ADD CONSTRAINT simpleassets_transfers_assets_transfers_fkey FOREIGN KEY (transfer_id) REFERENCES simpleassets_transfers(transfer_id) MATCH SIMPLE ON UPDATE RESTRICT ON DELETE RESTRICT DEFERRABLE INITIALLY DEFERRED NOT VALID;
 -- INDEXES --
 CREATE INDEX simpleassets_assets_asset_id ON simpleassets_assets USING btree (asset_id);
 CREATE INDEX simpleassets_assets_author_btree ON simpleassets_assets USING btree (author);
@@ -72,11 +66,9 @@ CREATE INDEX simpleassets_assets_burned_at_time ON simpleassets_assets USING btr
 CREATE INDEX simpleassets_assets_updated_at_time ON simpleassets_assets USING btree (updated_at_time);
 CREATE INDEX simpleassets_assets_transferred_at_time ON simpleassets_assets USING btree (transferred_at_time);
 CREATE INDEX simpleassets_assets_minted_at_time ON simpleassets_assets USING btree (minted_at_time);
-CREATE INDEX simpleassets_assets_immutable_data_gin ON simpleassets_assets USING gin (mutable_data);
+CREATE INDEX simpleassets_assets_mutable_data_gin ON simpleassets_assets USING gin (mutable_data);
 CREATE INDEX simpleassets_assets_immutable_data_gin ON simpleassets_assets USING gin (immutable_data);
-
 CREATE INDEX simpleassets_transfers_sender ON simpleassets_transfers USING btree (sender);
 CREATE INDEX simpleassets_transfers_recipient ON simpleassets_transfers USING btree (recipient);
 CREATE INDEX simpleassets_transfers_created_at_time ON simpleassets_transfers USING btree (created_at_time);
-
 CREATE INDEX simpleassets_transfers_assets_asset_id ON simpleassets_transfers_assets USING btree (asset_id);
